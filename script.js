0a2f8b1406b7cea84a068deee41943f916bcbf66
TecnnoBot03 <simoncarreno2006@gmail.com>
2022-11-23 19:07:09 -0500

    añadida la barrra de busqueda con sus estilos, agregados 3 productos mas para la prueba

--- script.js
+++ script.js
@@ -15,6 +15,24 @@ let herramientasVender = [
         nombre: "Martillo Neumatico",
         precio: 400,
         categoria: "martillos",
+        imagen: "asfsfbreb"
+    },
+    sierra = {
+        nombre: "Sierra de mesa",
+        precio: 600,
+        categoria: "sierras",
+        imagen: "asfsfbreb"
+    },
+    pulidora = {
+        nombre: "Pulidora de bateria",
+        precio: 150,
+        categoria: "pulidoras",
+        imagen: "asfsfbreb"
+    },
+    martilloNeumatico = {
+        nombre: "Martillo Neumatico",
+        precio: 400,
+        categoria: "martillos",
         iamgen: "asfsfbreb"
     }
 ];
